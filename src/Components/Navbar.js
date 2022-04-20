11df1bc785a1494893d2d9b17585d39363bb5f45
Adarsh Gaur <adarshgaur3@gmail.com>
2022-04-20 17:44:06 +0530

    added Navbar section

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -6,7 +6,7 @@ export default function Navbar(){
 		<nav className="navbar">
 			<img src={img} alt="troll-face"/>
 			<h1>Meme Generator</h1>
-			<h3>All Memes</h3>
+			<h3>React Project</h3>
 		</nav>
 	)
 }
